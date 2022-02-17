78a32404303e3fcf41cdd97415fa365f55a26013
Manar-Alzoubi <mzzo3bi20988@gmail.com>
2022-02-17 05:20:08 +0200

    last

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-
-
+import FavList from './components/FavList/FavList';
 import Home from './components/Home/Home';
-// import Fav from './components/favPage/FavPage';
 import  Navbar  from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-       {/* <Navbar/> */}
+       <Navbar/>
       <Routes>
        
           <Route path='/' exact  element={<Home/>} />
-          {/* <Route path='/favorite' exact element={<Fav/>} /> */}
+          <Route path='/favorite' exact element={<FavList/>} />
         
       </Routes>
     </div>
